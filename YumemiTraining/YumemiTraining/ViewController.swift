66358bb2160bb2c802f5601f1c6274edd8a54481
torakyun <leo@MacBook-Pro-3.local>
2022-02-18 18:13:53 +0900

    first commit

---
+++ YumemiTraining/YumemiTraining/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YumemiTraining
+//
+//  Created by 水野虎樹 on 2022/02/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
