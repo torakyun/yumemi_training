bf74d48ab5def45d8e0b5e61944b1e1e497a3394
torakyun <leo@MacBook-Pro-3.stama1.kt.home.ne.jp>
2022-02-22 18:08:46 +0900

    show weather image

--- YumemiTraining/YumemiTraining/ViewController.swift
+++ YumemiTraining/YumemiTraining/ViewController.swift
@@ -6,13 +6,62 @@
 //
 
 import UIKit
+import YumemiWeather
 
 class ViewController: UIViewController {
     
+    @IBOutlet private weak var weatherImageView: UIImageView!
+    private var weahterImage: UIImage?
+    private var weahterImageColor: UIColor?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.changeWeatherImage()
     }
 
+    @IBAction func reloadButtonDidPress(_ sender: Any) {
+        self.changeWeatherImage()
+    }
+    
+    private func changeWeatherImage() {
+        
+        let weatherImageData = self.fetchWeather()
+        
+        if let weahterImage = weatherImageData.image {
+            self.weahterImage = weahterImage
+        }
+        self.weatherImageView.image = self.weahterImage
+        
+        if let weahterImageColor = weatherImageData.color {
+            self.weahterImageColor = weahterImageColor
+        }
+        self.weatherImageView.tintColor = self.weahterImageColor
+        
+    }
+    
+    private func fetchWeather() -> (image: UIImage?, color: UIColor?) {
+        
+        let weather = YumemiWeather.fetchWeather()
+        var weatherImage: UIImage?
+        var weatherImageColor: UIColor?
+        
+        if weather == "sunny" {
+            weahterImage = UIImage(named: "sunny")
+            weatherImageColor = .red
+        } else if weather == "cloudy" {
+            weahterImage = UIImage(named: "cloudy")
+            weatherImageColor = .gray
+        } else if weather == "rainy" {
+            weahterImage = UIImage(named: "rainy")
+            weatherImageColor = .blue
+        } else {
+            weatherImage = nil
+            weatherImageColor = nil
+        }
+        
+        return (weatherImage, weatherImageColor)
+        
+    }
 }
 
