f459244fdf0de8a129bd301a90a17c5752b69f46
torakyun <leo@MacBook-Pro-3.local>
2022-03-02 11:15:55 +0900

    天気予報APIのエラーハンドリング

--- YumemiTraining/YumemiTraining/ViewController.swift
+++ YumemiTraining/YumemiTraining/ViewController.swift
@@ -42,22 +42,47 @@ class ViewController: UIViewController {
     
     private func fetchWeather() -> (image: UIImage?, color: UIColor?) {
         
-        let weather = YumemiWeather.fetchWeather()
         var weatherImage: UIImage?
         var weatherImageColor: UIColor?
         
-        if weather == "sunny" {
-            weahterImage = UIImage(named: "sunny")
-            weatherImageColor = .red
-        } else if weather == "cloudy" {
-            weahterImage = UIImage(named: "cloudy")
-            weatherImageColor = .gray
-        } else if weather == "rainy" {
-            weahterImage = UIImage(named: "rainy")
-            weatherImageColor = .blue
-        } else {
-            weatherImage = nil
-            weatherImageColor = nil
+        do {
+            
+            let weather = try YumemiWeather.fetchWeather(at: "tokyo")
+            
+            if weather == "sunny" {
+                weahterImage = UIImage(named: "sunny")
+                weatherImageColor = .red
+            } else if weather == "cloudy" {
+                weahterImage = UIImage(named: "cloudy")
+                weatherImageColor = .gray
+            } else if weather == "rainy" {
+                weahterImage = UIImage(named: "rainy")
+                weatherImageColor = .blue
+            } else {
+                weatherImage = nil
+                weatherImageColor = nil
+            }
+            
+        } catch YumemiWeatherError.invalidParameterError {
+            
+            let alert = UIAlertController(title: "天気情報の取得に失敗", message: "invalidParameterError", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel) { action in
+                
+            }
+            alert.addAction(cancelAction)
+            present(alert, animated: true)
+            
+        } catch YumemiWeatherError.unknownError {
+        
+            let alert = UIAlertController(title: "天気情報の取得に失敗", message: "unknownError", preferredStyle: .alert)
+            let cancelAction = UIAlertAction(title: "キャンセル", style: .cancel) { action in
+                
+            }
+            alert.addAction(cancelAction)
+            present(alert, animated: true)
+        
+        } catch let error {
+            print(error)
         }
         
         return (weatherImage, weatherImageColor)
