f2c469b01389b8bfcbce5ca3756bc0226f8e8874
dazy1030 <dev.nodajima@gmail.com>
2022-03-11 10:09:49 +0900

    feat: UI配置

--- YumemiTraining/YumemiTraining/SampleViewController.swift
+++ YumemiTraining/YumemiTraining/SampleViewController.swift
@@ -7,4 +7,18 @@
 
 import UIKit
 
-final class SampleViewController: UIViewController {}
+final class SampleViewController: UIViewController {
+    @IBOutlet private weak var sampleTextField: UITextField!
+    @IBOutlet private weak var sampleLabel: UILabel!
+    @IBOutlet private weak var sampleButton: UIButton!
+    @IBOutlet private weak var sampleImageView: UIImageView!
+}
+
+// MARK: - UITextFieldDelegate
+
+extension SampleViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
